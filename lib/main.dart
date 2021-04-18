ba4ce6d1efd6b59371987ab00a387c48cbe7f666
leoruas <leo.coelho.ruas@gmail.com>
2021-04-18 16:49:59 -0300

    added app connection with node server/socketio

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Socket socket;
+  String ip;
+  bool hasConnected = false;
+
+  @override
+  void initState() {
+    super.initState();
+    connectToServer();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,11 +31,37 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         body: Center(
           child: RaisedButton(
-            onPressed: null,
+            onPressed: hasConnected
+                ? () {
+                    socket.emit("open-game");
+                  }
+                : null,
             child: Text("Open Game"),
           ),
         ),
       ),
     );
+  }
+
+  void connectToServer() async {
+    await DotEnv.load(fileName: ".env"); //get env file
+    try {
+      ip = env['SERVER_IP']; //get ip from .env file
+
+      // Configure socket to connect with server ip
+      socket = io('http://$ip:8028', <String, dynamic>{
+        'transports': ['websocket'],
+        'autoConnect': false,
+      });
+
+      socket.connect();
+
+      socket.on('connect', (_) => print('Connect to socket with id: ${socket.id}'));
+      setState(() {
+        hasConnected = true;  
+      });
+    } catch (e) {
+      print(e.toString());
+    }
   }
 }
